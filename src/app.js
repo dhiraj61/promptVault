da64089814fd6426bd5afa6b19c95f1512d532de
Dhiraj chaudhari <chaudharydhiraj715@gmail.com>
2025-11-11 18:31:13 +0530

    Done

--- src/app.js
+++ src/app.js
@@ -5,12 +5,13 @@ const authRoute = require("./routes/authRoutes");
 const postRoute = require("./routes/postRoutes");
 const likeRoutes = require("./routes/likeRoutes");
 const app = express();
+const allowedOrigin = process.env.FRONTEND_URI || "http://localhost:5173";
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: process.env.FRONTEND_URI,
+    origin: allowedOrigin,
     credentials: true,
   })
 );
